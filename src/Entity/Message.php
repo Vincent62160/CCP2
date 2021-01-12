fd31e3222ea215131fecc4dd15c678854e9b0ac8
Duval Vincent <vincent-duval@bbox.fr>
2021-01-12 21:09:08 +0100

    dernier commit

---
+++ src/Entity/Message.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Sujet;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Commentaire;
+
+   
+   
+    
+    public function __construct()
+    {
+        $this->User = new ArrayCollection();
+        $this->Client = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->Sujet;
+    }
+
+    public function setSujet(string $Sujet): self
+    {
+        $this->Sujet = $Sujet;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->Commentaire;
+    }
+
+    public function setCommentaire(string $Commentaire): self
+    {
+        $this->Commentaire = $Commentaire;
+
+        return $this;
+    }
+
+}
